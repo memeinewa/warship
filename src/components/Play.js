700e78898c7cc1d7df9f901191f6fb26e54b6c2f
suphawat jutrasong <suphawat@hocco.co>
2022-04-08 02:51:01 +0700

    - [feat] add playing status

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,5 +1,5 @@
 import { Card } from 'react-bootstrap'
-import { collection, onSnapshot, setDoc, doc, getDoc, getDocs, query } from 'firebase/firestore'
+import { collection, onSnapshot, setDoc, doc, getDoc, getDocs, query, updateDoc } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 
 import { db } from '../firebase'
@@ -7,20 +7,31 @@ import { useAuth } from '../contexts/AuthContext'
 
 export default function Play() {
     const [snapStatus, setSnapStatus] = useState(false)
+    const [playStatus, setPlayStatus] = useState(false)
     const { value: { currentUser } } = useAuth()
     const player = currentUser.email.split("@")[0]
+    let isPlayStatus = false
 
     useEffect(() => {
         if (snapStatus) {
             const docRef = doc(db, 'waiting', player)
             onSnapshot(docRef, async (snapshot) => {
                 const hostData = snapshot.data()
+                if (hostData.guess) {
+                    setPlayStatus(true)
+                }
             })
         }
         else {
             room()
         }
     }, [snapStatus])
+
+    useEffect(() => {
+        if (playStatus) {
+            console.log('do play')
+        }
+    }, [playStatus])
 
     const room = async () => {
         const q = query(collection(db, 'waiting'))
@@ -51,9 +62,15 @@ export default function Play() {
         }
     }
 
-    const joiningRoom = (docs) => {
-        const randomRoom = Math.floor(Math.random() * docs.length)
-        const roomName = docs[randomRoom].id;
+    const joiningRoom = async (docs) => {
+        if (!isPlayStatus) {
+            const randomRoom = Math.floor(Math.random() * docs.length)
+            const roomName = docs[randomRoom].id
+            const docRef = doc(db, 'waiting', roomName)
+            await updateDoc(docRef, { guess: player })
+            isPlayStatus = true
+            setPlayStatus(true)
+        }
     }
 
     return (
